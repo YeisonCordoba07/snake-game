fc7727208ddd197aeed7658c16d0fc6c10edfc59
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2023-02-02 20:06:30 -0500

    arregla el movimiento del cuerpo de la serpiente

--- snake.js
+++ snake.js
@@ -3,12 +3,33 @@ const contextoCanvas = canvas.getContext("2d");
 
 const ancho = 20;
 const largo = 20;
-const velocidad = 5;
+const velocidad = 7;
+
+
+let manzana = {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+}
+
+let serpiente = {
+    numeroDeHijos: 0,
+    x: 10,
+    y: 10,
+    width: 50,
+    height: 50,   
+    hijos: [],
+}
+
+let direccion = "derecha";
+
+
+
 
 
 function gameLoop(){
     dibujar();
-
     moverSerpiente();
     intercepcion();
 }
@@ -38,27 +59,6 @@ function inicializar(){
 
 
 }
-
-
-let manzana = {
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-}
-
-let serpiente = {
-    numeroDeHijos: 0,
-    x: 10,
-    y: 10,
-    width: 50,
-    height: 50,   
-    hijos: [],
-}
-
-let direccion = "derecha";
-
-
 
 
 
@@ -103,31 +103,25 @@ function moverSerpiente(){
 
 function moverHijos(){
 
+    serpiente.hijos[0].x = serpiente.x;
+    serpiente.hijos[0].y = serpiente.y;
     for(let i = serpiente.hijos.length -1; i > 0; i--){
         serpiente.hijos[i].x = serpiente.hijos[i-1].x;
         serpiente.hijos[i].y = serpiente.hijos[i-1].y;
     }
 
-    serpiente.hijos[0].x = serpiente.x;
-    serpiente.hijos[0].y = serpiente.y;
 }
 
 
 
-
 function moverManzana(){
-    // return Math.random() * (max - min) + min; no incluye el maximo
-    let posicionX = 0;
-    let posicionY = 0;
 
     let maxAncho = Math.trunc(canvas.width / ancho);
     let maxLargo = Math.trunc(canvas.height / largo);
 
-    posicionX = ancho *(Math.floor(Math.random()*maxAncho));
-    posicionY = largo *(Math.floor(Math.random()*maxLargo));
+    manzana.x = ancho *(Math.floor(Math.random()*maxAncho));
+    manzana.y = largo *(Math.floor(Math.random()*maxLargo));
 
-    manzana.x = posicionX;
-    manzana.y = posicionY;
 }
 
 
@@ -144,58 +138,20 @@ function intercepcion(){
         agregarHijo();
     }    
 
-    /*if((serpiente.x > manzana.x + manzana.width &&
-        serpiente.x + serpiente.width < manzana.x &&
-        serpiente.y > manzana.y + manzana.height &&
-        serpiente.y + serpiente.width < manzana.y)){
-            
-            console.log("no toca");
-            contextoCanvas.clearRect(manzana.x, manzana.y, manzana.width, manzana.height);
-            moverManzana();
-        }else{
-            
-            console.log("si toca");
-        }*/
-
-    /*if((serpiente.x > manzana.x + manzana.width &&
-        serpiente.x + serpiente.width < manzana.x &&
-        serpiente.y > manzana.y + manzana.height &&
-        serpiente.y + serpiente.width < manzana.y)){
-            
-            contextoCanvas.clearRect(manzana.x, manzana.y, manzana.width, manzana.height);
-            moverManzana();
-        }*/
-
-   /* Si ( x1 > x2+w2 ) ==> No hay colisión
-Si ( x1+w1 < x2 ) ==> No hay colisión
-Si ( y1 > y2+h2 ) ==> No hay colisión
-Si ( y1+h1 < y2 ) ==> No hay colisión
-En otro caso ==> Hay colisión*/
 }
 
 
 
 
 function agregarHijo(){
-    serpiente.hijos[serpiente.numeroDeHijos] =  {
-        x: 0,
-        y: 0,
-    }
-    /*serpiente.hijos[serpiente.numeroDeHijos] =  {
-        x: serpiente.hijos[serpiente.numeroDeHijos-1].x - serpiente.width,
-        y: serpiente.hijos[serpiente.numeroDeHijos-1].y - serpiente.height,
-    }*/
 
-   /* serpiente.hijos[serpiente.numeroDeHijos].push({
-        x: serpiente.hijos[serpiente.numeroDeHijos-1].x - serpiente.width,
-        y: serpiente.hijos[serpiente.numeroDeHijos-1].y - serpiente.height,
-    });*/
+    serpiente.hijos[serpiente.numeroDeHijos] =  {
+        x: serpiente.hijos[serpiente.hijos.length -1].x,
+        y: serpiente.hijos[serpiente.hijos.length -1].y,
+    }
+
     serpiente.numeroDeHijos = serpiente.numeroDeHijos + 1;
-    console.log(`Tamaño: ${serpiente.hijos.length}`);
-    console.log(serpiente.hijos); 
-    serpiente.hijos.forEach(i => {
-        console.log(`i = ${i}`);
-    }); 
+
 }
 
 
@@ -215,6 +171,8 @@ function dibujar(){
         serpiente.width, 
         serpiente.height
         );
+
+
     //Dibuja la hijos
     contextoCanvas.fillStyle = "lightgreen";
 
@@ -228,8 +186,6 @@ function dibujar(){
             );
 
     }
-
-
 
 
     //Dibuja la manzana
